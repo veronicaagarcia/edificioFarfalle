582bf1ac270db1eafad32ef0464539c5e69c852e
veronicaagarcia <veroagarcia90@gmail.com>
2025-06-04 18:31:04 -0300

    Fix FontAwesome icons - build working

--- src/components/comon/DeptoModel.tsx
+++ src/components/comon/DeptoModel.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowLeft, faArrowRight, faTimes } from "@fortawesome/free-solid-svg-icons"
-import type { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 interface DeptoModelProps {
   numVideos: number
@@ -81,7 +80,7 @@ export function DeptoModel({ imageNames, videoNames, initialIndex, onClose }: De
               disabled={selectedIndex === 0}
               aria-label="Imagen anterior"
             >
-              <FontAwesomeIcon icon={faArrowLeft as IconProp} className={selectedIndex === 0 ? "opacity-30" : "opacity-100"} />
+              <FontAwesomeIcon icon={faArrowLeft} className={selectedIndex === 0 ? "opacity-30" : "opacity-100"} />
             </button>
 
             <div className="relative flex items-center justify-center w-10/12 md:w-5/12 h-auto">
@@ -118,7 +117,7 @@ export function DeptoModel({ imageNames, videoNames, initialIndex, onClose }: De
               aria-label="Imagen siguiente"
             >
               <FontAwesomeIcon
-                icon={faArrowRight as IconProp}
+                icon={faArrowRight}
                 className={selectedIndex === imageNames.length + videoNames.length - 1 ? "opacity-30" : "opacity-100"}
               />
             </button>
@@ -129,7 +128,7 @@ export function DeptoModel({ imageNames, videoNames, initialIndex, onClose }: De
             onClick={handleExitFullscreen}
             aria-label="Cerrar"
           >
-            <FontAwesomeIcon icon={faTimes as IconProp} />
+            <FontAwesomeIcon icon={faTimes} />
           </button>
 
           <div className="absolute bottom-4 left-0 right-0 text-center text-white">
